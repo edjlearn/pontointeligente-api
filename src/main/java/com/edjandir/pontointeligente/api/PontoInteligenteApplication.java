0f525633a7fc1e81c3bfa22a5e3695d75aa03d0a
Edjandir <edjlearn@gmail.com>
2021-09-19 20:58:31 -0300

    Adicionando os controllers

--- src/main/java/com/edjandir/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/edjandir/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,6 +3,7 @@ package com.edjandir.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PontoInteligenteApplication {
 
